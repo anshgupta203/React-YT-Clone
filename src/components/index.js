18d197853cf38e40bf7c866667d088c5a4f9ba3b
anshgupta203 <56402745+anshgupta203@users.noreply.github.com>
2022-12-07 23:09:07 +0530

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+/**
+ * How we call all the exports of file into one file so we don't clutter the App.js 
+ * with so many import calls for different components
+ */
+
+export { default as Navbar } from './Navbar'
+export { default as Feed } from './Feed'
+export { default as SearchFeed } from './SearchFeed'
+export { default as VideoDetail } from './VideoDetail'
+export { default as ChannelDetail } from './ChannelDetail'
+export { default as SearchBar } from './SearchBar'
+export { default as Sidebar } from './Sidebar'
+export { default as Videos } from './Videos'
+export { default as VideoCard } from './VideoCard'
+export { default as ChannelCard } from './ChannelCard'
+
